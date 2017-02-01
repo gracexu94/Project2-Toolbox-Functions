3172af3d751446491c9304e32c8045694fad4c6e
gracexu94 <serihppas@gmail.com>
2017-02-01 02:06:56 -0500

    curve controls added

--- src/main.js
+++ src/main.js
@@ -21,7 +21,32 @@ function degreesToRads(degrees) {
 var xAxis = new THREE.Vector3(1,0,0);
 var yAxis = new THREE.Vector3(0,1,0);
 var zAxis = new THREE.Vector3(0,0,1);
-var guiVars = {numFeathers: 45, 
+var guiVars = {
+            leftWingCurve_startPtX: -0.1,
+            leftWingCurve_startPtY: 0.0,
+            leftWingCurve_startPtZ: 0.0,
+            leftWingCurve_Pt1X: -1.0,
+            leftWingCurve_Pt1Y: 0.75,
+            leftWingCurve_Pt1Z: 0.0,
+            leftWingCurve_Pt2X: -2.0,
+            leftWingCurve_Pt2Y: 3.0,
+            leftWingCurve_Pt2Z: 0.0,
+            leftWingCurve_endPtX: -5.0,
+            leftWingCurve_endPtY: 2.0,
+            leftWingCurve_endPtZ: 0.0,
+            rightWingCurve_startPtX: 0.1,
+            rightWingCurve_startPtY: 0.0,
+            rightWingCurve_startPtZ: 0.0,
+            rightWingCurve_Pt1X: 1.0,
+            rightWingCurve_Pt1Y: 0.75,
+            rightWingCurve_Pt1Z: 0.0,
+            rightWingCurve_Pt2X: 2.0,
+            rightWingCurve_Pt2Y: 3.0,
+            rightWingCurve_Pt2Z: 0.0,
+            rightWingCurve_endPtX: 5.0,
+            rightWingCurve_endPtY: 2.0,
+            rightWingCurve_endPtZ: 0.0,
+            numFeathers: 45, 
             windStrength: 3.0, 
             featherSize: 1.0, 
             featherColorR_start: 0.0,
@@ -29,7 +54,13 @@ var guiVars = {numFeathers: 45,
             featherColorB_start: 1.0,
             featherColorR_end: 1.0,
             featherColorG_end: 1.0,
-            featherColorB_end: 1.0  };
+            featherColorB_end: 1.0,
+            xRotationLocal_degrees: 0.0,
+            yRotationLocal_degrees: 0.0,
+            zRotationLocal_degrees: 0.0,
+            xRotationGlobal_degrees: 0.0,
+            yRotationGlobal_degrees: 0.0,
+            zRotationGlobal_degrees: 0.0};
 
 // called after the scene loads
 function onLoad(framework) {
@@ -110,30 +141,244 @@ function onLoad(framework) {
         removeWings(framework.scene);
         createWings(framework.scene, rightWingObject, leftWingObject);
     });
-    gui.add(guiVars, 'featherColorR_start', 0.0, 1.0).onChange(function(newVal) {
+
+    var rightCurveFolder = gui.addFolder('Right wing curve controls');
+    rightCurveFolder.add(guiVars, 'rightWingCurve_startPtX', -10.0, 10.0).onChange(function(newVal) {
+        removeWings(framework.scene);
+        var updatedRightWing = updateRightWingCurve(framework.scene);
+        createWings(framework.scene, updatedRightWing, leftWingObject);
+    });
+    rightCurveFolder.add(guiVars, 'rightWingCurve_startPtY', -10.0, 10.0).onChange(function(newVal) {
+        removeWings(framework.scene);
+        var updatedRightWing = updateRightWingCurve(framework.scene);
+        createWings(framework.scene, updatedRightWing, leftWingObject);
+    });
+    rightCurveFolder.add(guiVars, 'rightWingCurve_startPtZ', -10.0, 10.0).onChange(function(newVal) {
+        removeWings(framework.scene);
+        var updatedRightWing = updateRightWingCurve(framework.scene);
+        createWings(framework.scene, updatedRightWing, leftWingObject);
+    });
+    rightCurveFolder.add(guiVars, 'rightWingCurve_Pt1X', -10.0, 10.0).onChange(function(newVal) {
+        removeWings(framework.scene);
+        var updatedRightWing = updateRightWingCurve(framework.scene);
+        createWings(framework.scene, updatedRightWing, leftWingObject);
+    });
+    rightCurveFolder.add(guiVars, 'rightWingCurve_Pt1Y', -10.0, 10.0).onChange(function(newVal) {
+        removeWings(framework.scene);
+        var updatedRightWing = updateRightWingCurve(framework.scene);
+        createWings(framework.scene, updatedRightWing, leftWingObject);
+    });
+    rightCurveFolder.add(guiVars, 'rightWingCurve_Pt1Z', -10.0, 10.0).onChange(function(newVal) {
+        removeWings(framework.scene);
+        var updatedRightWing = updateRightWingCurve(framework.scene);
+        createWings(framework.scene, updatedRightWing, leftWingObject);
+    });
+    rightCurveFolder.add(guiVars, 'rightWingCurve_Pt2X', -10.0, 10.0).onChange(function(newVal) {
+        removeWings(framework.scene);
+        var updatedRightWing = updateRightWingCurve(framework.scene);
+        createWings(framework.scene, updatedRightWing, leftWingObject);
+    });
+    rightCurveFolder.add(guiVars, 'rightWingCurve_Pt2Y', -10.0, 10.0).onChange(function(newVal) {
+        removeWings(framework.scene);
+        var updatedRightWing = updateRightWingCurve(framework.scene);
+        createWings(framework.scene, updatedRightWing, leftWingObject);
+    });
+    rightCurveFolder.add(guiVars, 'rightWingCurve_Pt2Z', -10.0, 10.0).onChange(function(newVal) {
+        removeWings(framework.scene);
+        var updatedRightWing = updateRightWingCurve(framework.scene);
+        createWings(framework.scene, updatedRightWing, leftWingObject);
+    });
+    rightCurveFolder.add(guiVars, 'rightWingCurve_endPtX', -10.0, 10.0).onChange(function(newVal) {
+        removeWings(framework.scene);
+        var updatedRightWing = updateRightWingCurve(framework.scene);
+        createWings(framework.scene, updatedRightWing, leftWingObject);
+    });
+    rightCurveFolder.add(guiVars, 'rightWingCurve_endPtY', -10.0, 10.0).onChange(function(newVal) {
+        removeWings(framework.scene);
+        var updatedRightWing = updateRightWingCurve(framework.scene);
+        createWings(framework.scene, updatedRightWing, leftWingObject);
+    });
+    rightCurveFolder.add(guiVars, 'rightWingCurve_endPtZ', -10.0, 10.0).onChange(function(newVal) {
+        removeWings(framework.scene);
+        var updatedRightWing = updateRightWingCurve(framework.scene);
+        createWings(framework.scene, updatedRightWing, leftWingObject);
+    });
+
+    var leftCurveFolder = gui.addFolder('Left wing curve controls');
+    leftCurveFolder.add(guiVars, 'leftWingCurve_startPtX', -10.0, 10.0).onChange(function(newVal) {
+        removeWings(framework.scene);
+        var updatedLeftWing = updateLeftWingCurve(framework.scene);
+        createWings(framework.scene, rightWingObject, updatedLeftWing);
+    });
+    leftCurveFolder.add(guiVars, 'leftWingCurve_startPtY', -10.0, 10.0).onChange(function(newVal) {
+        removeWings(framework.scene);
+        var updatedLeftWing = updateLeftWingCurve(framework.scene);
+        createWings(framework.scene, rightWingObject, updatedLeftWing);
+    });
+    leftCurveFolder.add(guiVars, 'leftWingCurve_startPtZ', -10.0, 10.0).onChange(function(newVal) {
+        removeWings(framework.scene);
+        var updatedLeftWing = updateLeftWingCurve(framework.scene);
+        createWings(framework.scene, rightWingObject, updatedLeftWing);
+    });
+    leftCurveFolder.add(guiVars, 'leftWingCurve_Pt1X', -10.0, 10.0).onChange(function(newVal) {
+        removeWings(framework.scene);
+        var updatedLeftWing = updateLeftWingCurve(framework.scene);
+        createWings(framework.scene, rightWingObject, updatedLeftWing);
+    });
+    leftCurveFolder.add(guiVars, 'leftWingCurve_Pt1Y', -10.0, 10.0).onChange(function(newVal) {
+        removeWings(framework.scene);
+        var updatedLeftWing = updateLeftWingCurve(framework.scene);
+        createWings(framework.scene, rightWingObject, updatedLeftWing);
+    });
+    leftCurveFolder.add(guiVars, 'leftWingCurve_Pt1Z', -10.0, 10.0).onChange(function(newVal) {
+        removeWings(framework.scene);
+        var updatedLeftWing = updateLeftWingCurve(framework.scene);
+        createWings(framework.scene, rightWingObject, updatedLeftWing);
+    });
+    leftCurveFolder.add(guiVars, 'leftWingCurve_Pt2X', -10.0, 10.0).onChange(function(newVal) {
+        removeWings(framework.scene);
+        var updatedLeftWing = updateLeftWingCurve(framework.scene);
+        createWings(framework.scene, rightWingObject, updatedLeftWing);
+    });
+    leftCurveFolder.add(guiVars, 'leftWingCurve_Pt2Y', -10.0, 10.0).onChange(function(newVal) {
+        removeWings(framework.scene);
+        var updatedLeftWing = updateLeftWingCurve(framework.scene);
+        createWings(framework.scene, rightWingObject, updatedLeftWing);
+    });
+    leftCurveFolder.add(guiVars, 'leftWingCurve_Pt2Z', -10.0, 10.0).onChange(function(newVal) {
+        removeWings(framework.scene);
+        var updatedLeftWing = updateLeftWingCurve(framework.scene);
+        createWings(framework.scene, rightWingObject, updatedLeftWing);
+    });
+    leftCurveFolder.add(guiVars, 'leftWingCurve_endPtX', -10.0, 10.0).onChange(function(newVal) {
+        removeWings(framework.scene);
+        var updatedLeftWing = updateLeftWingCurve(framework.scene);
+        createWings(framework.scene, rightWingObject, updatedLeftWing);
+    });
+    leftCurveFolder.add(guiVars, 'leftWingCurve_endPtY', -10.0, 10.0).onChange(function(newVal) {
+        removeWings(framework.scene);
+        var updatedLeftWing = updateLeftWingCurve(framework.scene);
+        createWings(framework.scene, rightWingObject, updatedLeftWing);
+    });
+    leftCurveFolder.add(guiVars, 'leftWingCurve_endPtZ', -10.0, 10.0).onChange(function(newVal) {
+        removeWings(framework.scene);
+        var updatedLeftWing = updateLeftWingCurve(framework.scene);
+        createWings(framework.scene, rightWingObject, updatedLeftWing);
+    });
+
+    var colorFolder = gui.addFolder('Color controls');
+    colorFolder.add(guiVars, 'featherColorR_start', 0.0, 1.0).onChange(function(newVal) {
         removeWings(framework.scene);
         createWings(framework.scene, rightWingObject, leftWingObject);
     });
-    gui.add(guiVars, 'featherColorR_end', 0.0, 1.0).onChange(function(newVal) {
+    colorFolder.add(guiVars, 'featherColorR_end', 0.0, 1.0).onChange(function(newVal) {
         removeWings(framework.scene);
         createWings(framework.scene, rightWingObject, leftWingObject);
     });
-    gui.add(guiVars, 'featherColorG_start', 0.0, 1.0).onChange(function(newVal) {
+    colorFolder.add(guiVars, 'featherColorG_start', 0.0, 1.0).onChange(function(newVal) {
         removeWings(framework.scene);
         createWings(framework.scene, rightWingObject, leftWingObject);
     });
-    gui.add(guiVars, 'featherColorG_end', 0.0, 1.0).onChange(function(newVal) {
+    colorFolder.add(guiVars, 'featherColorG_end', 0.0, 1.0).onChange(function(newVal) {
         removeWings(framework.scene);
         createWings(framework.scene, rightWingObject, leftWingObject);
     });
-    gui.add(guiVars, 'featherColorB_start', 0.0, 1.0).onChange(function(newVal) {
+    colorFolder.add(guiVars, 'featherColorB_start', 0.0, 1.0).onChange(function(newVal) {
         removeWings(framework.scene);
         createWings(framework.scene, rightWingObject, leftWingObject);
     });
-    gui.add(guiVars, 'featherColorB_end', 0.0, 1.0).onChange(function(newVal) {
+    colorFolder.add(guiVars, 'featherColorB_end', 0.0, 1.0).onChange(function(newVal) {
         removeWings(framework.scene);
         createWings(framework.scene, rightWingObject, leftWingObject);
     });
+
+    var orientationFolder = gui.addFolder('Feather orientation controls');
+    orientationFolder.add(guiVars, 'xRotationLocal_degrees', 0.0, 360.0).onChange(function(newVal) {
+        rotateWingsLocal(scene);
+    });
+    orientationFolder.add(guiVars, 'yRotationLocal_degrees', 0.0, 360.0).onChange(function(newVal) {
+        rotateWingsLocal(scene);
+    });
+    orientationFolder.add(guiVars, 'zRotationLocal_degrees', 0.0, 360.0).onChange(function(newVal) {
+        rotateWingsLocal(scene);
+    });
+    orientationFolder.add(guiVars, 'xRotationGlobal_degrees', 0.0, 360.0).onChange(function(newVal) {
+        rotateWingsGlobal(scene);
+    });
+    orientationFolder.add(guiVars, 'yRotationGlobal_degrees', 0.0, 360.0).onChange(function(newVal) {
+        rotateWingsGlobal(scene);
+    });
+    orientationFolder.add(guiVars, 'zRotationGlobal_degrees', 0.0, 360.0).onChange(function(newVal) {
+        rotateWingsGlobal(scene);
+    });
+}
+
+function updateRightWingCurve(scene) {
+    var rightWingCurve = scene.getObjectByName("rightWingCurve");
+    scene.remove(rightWingCurve);
+    var newRightWing = new THREE.CubicBezierCurve3(
+        new THREE.Vector3(guiVars.rightWingCurve_startPtX, guiVars.rightWingCurve_startPtY, guiVars.rightWingCurve_startPtZ),
+        new THREE.Vector3(guiVars.rightWingCurve_Pt1X, guiVars.rightWingCurve_Pt1Y, guiVars.rightWingCurve_Pt1Z),
+        new THREE.Vector3(guiVars.rightWingCurve_Pt2X, guiVars.rightWingCurve_Pt2Y, guiVars.rightWingCurve_Pt2Z),
+        new THREE.Vector3(guiVars.rightWingCurve_endPtX, guiVars.rightWingCurve_endPtY, guiVars.rightWingCurve_endPtZ)
+    );
+
+    var rightWingGeom = new THREE.Geometry();
+    rightWingGeom.vertices = newRightWing.getPoints(guiVars.numFeathers);
+    var lineMaterial = new THREE.LineBasicMaterial();
+    var rightWingObject = new THREE.Line(rightWingGeom, lineMaterial);
+    rightWingObject.name = "rightWingCurve";
+    scene.add(rightWingObject);
+    return rightWingCurve;
+}
+
+function updateLeftWingCurve(scene) {
+    var leftWingCurve = scene.getObjectByName("leftWingCurve");
+    scene.remove(leftWingCurve);
+    var newLeftWing = new THREE.CubicBezierCurve3(
+        new THREE.Vector3(guiVars.leftWingCurve_startPtX, guiVars.leftWingCurve_startPtY, guiVars.leftWingCurve_startPtZ),
+        new THREE.Vector3(guiVars.leftWingCurve_Pt1X, guiVars.leftWingCurve_Pt1Y, guiVars.leftWingCurve_Pt1Z),
+        new THREE.Vector3(guiVars.leftWingCurve_Pt2X, guiVars.leftWingCurve_Pt2Y, guiVars.leftWingCurve_Pt2Z),
+        new THREE.Vector3(guiVars.leftWingCurve_endPtX, guiVars.leftWingCurve_endPtY, guiVars.leftWingCurve_endPtZ)
+    );
+
+    var leftWingGeom = new THREE.Geometry();
+    leftWingGeom.vertices = newLeftWing.getPoints(guiVars.numFeathers);
+    var lineMaterial = new THREE.LineBasicMaterial();
+    var leftWingObject = new THREE.Line(leftWingGeom, lineMaterial);
+    leftWingObject.name = "leftWingCurve";
+    scene.add(leftWingObject);
+    return leftWingCurve;
+}
+
+function rotateWingsLocal(scene) {
+    for (var i = 0; i < guiVars.numFeathers; i++) {
+        var rightFeather = scene.getObjectByName("rightFeather"+i);
+        var leftFeather = scene.getObjectByName("leftFeather"+i);
+        if (leftFeather !== undefined && rightFeather !== undefined) {
+            rightFeather.rotateX(degreesToRads(guiVars.xRotationLocal_degrees));
+            leftFeather.rotateX(degreesToRads(guiVars.xRotationLocal_degrees)); 
+            rightFeather.rotateY(degreesToRads(guiVars.yRotationLocal_degrees));
+            leftFeather.rotateY(degreesToRads(guiVars.yRotationLocal_degrees)); 
+            rightFeather.rotateZ(degreesToRads(guiVars.zRotationLocal_degrees));
+            leftFeather.rotateZ(degreesToRads(guiVars.zRotationLocal_degrees));        
+        } 
+    }
+}
+
+function rotateWingsGlobal(scene) {
+    for (var i = 0; i < guiVars.numFeathers; i++) {
+        var rightFeather = scene.getObjectByName("rightFeather"+i);
+        var leftFeather = scene.getObjectByName("leftFeather"+i);
+        if (leftFeather !== undefined && rightFeather !== undefined) {
+            rightFeather.rotateOnAxis(xAxis, degreesToRads(guiVars.xRotationGlobal_degrees));
+            leftFeather.rotateOnAxis(xAxis, degreesToRads(guiVars.xRotationGlobal_degrees)); 
+            rightFeather.rotateOnAxis(yAxis, degreesToRads(guiVars.yRotationGlobal_degrees));
+            leftFeather.rotateOnAxis(yAxis, degreesToRads(guiVars.yRotationGlobal_degrees)); 
+            rightFeather.rotateOnAxis(zAxis, degreesToRads(guiVars.zRotationGlobal_degrees));
+            leftFeather.rotateOnAxis(zAxis, degreesToRads(guiVars.zRotationGlobal_degrees));        
+        } 
+    }
 }
 
 function removeWings(scene) {
