2cb5bfd439c122b85e1716e32f4d1c5c8262f063
gracexu94 <serihppas@gmail.com>
2017-01-31 23:55:07 -0500

    right and left wing geometry added

--- src/main.js
+++ src/main.js
@@ -29,8 +29,6 @@ function onLoad(framework) {
     var gui = framework.gui;
     var stats = framework.stats;
 
-
-
     // Set light
     var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
     directionalLight.color.setHSL(0.1, 1, 0.95);
@@ -48,49 +46,77 @@ function onLoad(framework) {
     ] );
 
     scene.background = skymap;
-    var curve = new THREE.CubicBezierCurve3(
-        new THREE.Vector3( 0, 0, 0 ),
+    var lineMaterial = new THREE.LineBasicMaterial();
+    var numFeathers = 45;
+    //right wing
+    var rightWing = new THREE.CubicBezierCurve3(
+        new THREE.Vector3( 0.1, 0, 0 ),
         new THREE.Vector3( 1, 0.75, 0 ),
         new THREE.Vector3( 2, 3, 0 ),
         new THREE.Vector3( 5, 2, 0 )
     );
 
-    var geometry = new THREE.Geometry();
-    geometry.vertices = curve.getPoints(45);
+    var rightWingGeom = new THREE.Geometry();
+    rightWingGeom.vertices = rightWing.getPoints(numFeathers);
+    var rightWingObject = new THREE.Line(rightWingGeom, lineMaterial );
+    scene.add(rightWingObject);
 
-    var material = new THREE.LineBasicMaterial();
+    //legt wing
+    var leftWing = new THREE.CubicBezierCurve3(
+        new THREE.Vector3( -0.1, 0, 0 ),
+        new THREE.Vector3( -1, 0.75, 0 ),
+        new THREE.Vector3( -2, 3, 0 ),
+        new THREE.Vector3( -5, 2, 0 )
+    );
+
+    var leftWingGeom = new THREE.Geometry();
+    leftWingGeom.vertices = leftWing.getPoints(numFeathers);
 
     // Create the final object to add to the scene
-    var curveObject = new THREE.Line( geometry, material );
-    scene.add(curveObject);
+    var leftWingObject = new THREE.Line(leftWingGeom, lineMaterial);
+    scene.add(leftWingObject);
 
     // load a simple obj mesh
     var objLoader = new THREE.OBJLoader();
     objLoader.load('/geo/feather.obj', function(obj) {
-        for (var i = 0; i < geometry.vertices.length; i++) {
+        for (var i = 0; i < numFeathers; i++) {
             // LOOK: This function runs after the obj has finished loading
             var featherGeo = obj.children[0].geometry;
-            // Wing material
-            // interpolates between #5e4fa2, #d1836b
-            var interpolateFactor = linearInterpolate(1, 0, i/geometry.vertices.length);
+            
+            // Wing material: color interpolation
+            var interpolateFactor = linearInterpolate(1, 0, i/numFeathers);
             var featherMaterial = new THREE.MeshPhongMaterial();
             featherMaterial.color.setRGB(interpolateFactor, 1, 1);
-            console.log(featherMaterial);
 
-            var featherMesh = new THREE.Mesh(featherGeo, featherMaterial);
-            featherMesh.name = "feather" + i;
-            var scaleAmt = linearInterpolate(0.25, 1.0, i/geometry.vertices.length);
-            featherMesh.scale.x = scaleAmt;
-            featherMesh.scale.y = scaleAmt;
-            featherMesh.scale.z = scaleAmt;
+            var rightFeatherMesh = new THREE.Mesh(featherGeo, featherMaterial);
+            rightFeatherMesh.name = "rightFeather" + i;
+            var leftFeatherMesh = new THREE.Mesh(featherGeo, featherMaterial);
+            leftFeatherMesh.name = "leftFeather" + i;
 
-            featherMesh.position.set(geometry.vertices[i].x,geometry.vertices[i].y,geometry.vertices[i].z);
-            var zRotateAmt = linearInterpolate(270, 360, i/geometry.vertices.length);
-            featherMesh.rotateOnAxis(zAxis, degreesToRads(zRotateAmt));            
-            var xRotateAmt = linearInterpolate(80, 90, i/geometry.vertices.length);
-            featherMesh.rotateOnAxis(xAxis, degreesToRads(xRotateAmt));
+            // scale interpolation
+            var scaleAmt = linearInterpolate(0.25, 1.0, i/numFeathers);
+            rightFeatherMesh.scale.x = scaleAmt;
+            rightFeatherMesh.scale.y = scaleAmt;
+            rightFeatherMesh.scale.z = scaleAmt;
+            leftFeatherMesh.scale.x = scaleAmt;
+            leftFeatherMesh.scale.y = scaleAmt;
+            leftFeatherMesh.scale.z = scaleAmt;
 
-            scene.add(featherMesh);
+            // orientation interpolation
+            rightFeatherMesh.position.set(rightWingGeom.vertices[i].x,rightWingGeom.vertices[i].y,rightWingGeom.vertices[i].z);
+            var zRotateAmt = linearInterpolate(270, 360, i/numFeathers);
+            rightFeatherMesh.rotateOnAxis(zAxis, degreesToRads(zRotateAmt));            
+            var xRotateAmt = linearInterpolate(80, 90, i/numFeathers);
+            rightFeatherMesh.rotateOnAxis(xAxis, degreesToRads(xRotateAmt));
+            //rightFeatherMesh.rotateZ(degreesToRads(90));
+
+            leftFeatherMesh.position.set(leftWingGeom.vertices[i].x,leftWingGeom.vertices[i].y,leftWingGeom.vertices[i].z);
+            zRotateAmt = linearInterpolate(270, 180, i/numFeathers);
+            leftFeatherMesh.rotateOnAxis(zAxis, degreesToRads(zRotateAmt));            
+            leftFeatherMesh.rotateOnAxis(xAxis, degreesToRads(xRotateAmt));
+
+            scene.add(rightFeatherMesh);
+            scene.add(leftFeatherMesh);
         }
     });
 
